6e170f3bb93736c9733ce769c16d506db020ab7a
jdoan21 <jdoan21@stanford.edu>
2019-12-05 03:44:45 -0800

    create jsons for student IDs

--- generate/scores.py
+++ generate/scores.py
@@ -27,10 +27,10 @@ class Transitions:
                     rub1 = self.rubric_data[str(sub1)]
                     rub2 = self.rubric_data[str(sub2)]
                     tList = [self.scores[int(rub1[x])][int(rub2[x])] for x in range(len(rub1))]
-                    score = 0
+                    score = 0.0
                     for j in range(len(tList)):
                         score += (self.label_weights[j] * tList[j])
-                    self.tScores[s].append(score)
+                    self.tScores[s].append(round(score, 1))
 
                 self.maxScoreToID[round(max(self.tScores[s]), 1)].append(s)
                 self.minScoreToID[round(min(self.tScores[s]), 1)].append(s)
@@ -44,6 +44,9 @@ class Transitions:
             y.append(len(self.maxScoreToID[round(i/2, 1)]))
         plt.bar(np.array(x), np.array(y))
         # plt.xticks(np.array(x))
+        plt.xlabel('Max Transition Score')
+        plt.ylabel('Number of Students')
+        plt.title('p' + str(self.problem) + ' maximum transition score for each student')
         plt.savefig('generated/breakthroughBarGraph' + str(self.problem) + '.png')
         plt.clf()
 
@@ -54,9 +57,28 @@ class Transitions:
             x.append(round(i/2, 1))
             y.append(len(self.minScoreToID[round(i/2, 1)]))
         plt.bar(np.array(x), np.array(y))
+        plt.xlabel('Min Transition Score')
+        plt.ylabel('Number of Students')
+        plt.title('p' + str(self.problem) + ' minimum transition score for each student')
         # plt.xticks(np.array(x))
         plt.savefig('generated/lowPointsBarGraph' + str(self.problem) + '.png')
         plt.clf()
+
+    def saveMinIDs(self):
+        with open(f'generated/minIDs-{self.problem}.json', 'w') as dest_file:
+            minIDs = []
+            for i in range(-len(self.label_weights), 2*len(self.label_weights), 1):
+                if round(i/2, 1) in minIDs:
+                    minIDs += self.minScoreToID[round(i/2, 1)]
+            json.dump(minIDs, dest_file, indent=2)
+
+    def saveMaxIDs(self):
+        with open(f'generated/maxIDs-{self.problem}.json', 'w') as dest_file:
+            maxIDs = []
+            for i in range(2*len(self.label_weights), -len(self.label_weights), -1):
+                if round(i/2, 1) in self.maxScoreToID:
+                    maxIDs += self.maxScoreToID[round(i/2, 1)]
+            json.dump(maxIDs, dest_file, indent=2)
 
 
     # def generateBacktrackGraphs(self):
@@ -74,6 +96,9 @@ if __name__ == '__main__':
                 t.generateTransitionScores()
                 t.generateBreakthroughGraphs()
                 t.generateLowPointsGraphs()
+                t.saveMinIDs()
+                t.saveMaxIDs()
+                print('Finished p' + str(problem))
 
 
 
